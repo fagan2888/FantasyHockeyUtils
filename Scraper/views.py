3c5ae8156365b5dddaeef9ec68081858133b3805
Jordan Roth <Jordan-AR@hotmail.com>
2018-12-14 19:42:45 -0800

    Update views.py

--- Scraper/views.py
+++ Scraper/views.py
@@ -97,18 +97,20 @@ def scrape_players_for_team(request, team_id):
         nhl_team = NHLTeam.objects.get(espn_team_id=scraped_player['espn_nhl_team_id'])
         try:
             current_position = Position.objects.get(espn_position_id=scraped_player["espn_position_id"])
-            Player.objects.update_or_create(
-                name=scraped_player["name"],
-                nhl_team=nhl_team,
-                defaults={"fantasy_team": fantasy_team, "current_position": current_position}
-            )
+            player = Player()
+            player.name = name=scraped_player["name"]
+            player.nhl_team = nhl_team
+            player.fantasy_team = fantasy_team
+            player.current_position = current_position
+            player.save()
         except Position.DoesNotExist:
-            Player.objects.filter(name=scraped_player["name"], nhl_team=nhl_team).delete()
+            pass
 
     target_url = settings.CLUBHOUSE_URL.format(team_id=team_id)
     response_dict = requests.get(target_url).json()
     players = get_players_from_espn_response(response_dict)
     team = FantasyTeam.objects.get(team_id=team_id)
+    Player.objects.filter(fantasy_team=team).delete()
     for player in players:
         update_player_in_database(player, team)
 
