68c09d28e36e9885894a209f05ddb3d359b18832
Jordan Roth <Jordan-AR@hotmail.com>
2018-12-14 19:08:23 -0800

    Update urls.py

--- FantasyHockeyGamesTracker/urls.py
+++ FantasyHockeyGamesTracker/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^', include('TeamList.urls', namespace='team_list')),
     url(r'^admin/', admin.site.urls),
